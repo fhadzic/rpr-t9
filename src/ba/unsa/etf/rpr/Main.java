c2e7747dd036138748542d23adb117a1402ac5c8
fhadzic <fhadzic1@etf.unsa.ba>
2018-12-19 23:19:57 +0100

    Priprema...

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,12 +1,40 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static String ispisiGradove() throws SQLException {
+
+        GeografijaDAO geografija = GeografijaDAO.getInstance();
+
+        String string = "";
+        for (Grad grad : geografija.gradovi()) {
+            string += grad.getNaziv() + " (" + grad.getDrzava().getNaziv() + ") - " + grad.getBroj_stanovnika() + "\n";
+        }
+
+        return string;
+    }
+
+    private static void glavniGrad() throws SQLException {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Unesite naziv države");
+        String drzava = keyboard.nextLine();
+
+        Grad grad = GeografijaDAO.getInstance().glavniGrad(drzava);
+
+        if (grad != null) {
+            System.out.println("Glavni grad države " + drzava + " je " + grad.getNaziv());
+        } else {
+            System.out.println("Nepostojeća država");
+        }
+    }
+
+    public static void main(String[] args) throws SQLException{
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
     }
+
 }
